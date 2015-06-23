95a9047a81a157c7ef369e68a6305ce6f4349de1
nakashima <pixy2001@gmail.com>
2015-06-24 01:09:10 +0900

    implement handlers

--- handlers.py
+++ handlers.py
@@ -1,8 +1,12 @@
 import json
 import random
 import re
+import logging
+
+log = logging.getLogger(__name__)
 
 def load_from_config(section):
+    log.debug('loading %s', section)
     if section.get('type') == 'json':
         filepath = section['file_path']
         return JSONDefinedHandler(filepath)
@@ -10,16 +14,20 @@ def load_from_config(section):
 
 class DictDefinedHandler(object):
     def __init__(self, definition={}):
+        log.debug('constructing DictDefinedHandler: %s', definition)
         self.pattern = re.compile(definition.get('pattern', '*'))
         self.candidates = definition.get('candidates', [])
         self.default = definition.get('default', {})
 
     def handle(self, text):
+        log.debug('handling: %s', text)
         if self.pattern.match(text) is None:
             return None
+        log.debug('match: %s', text)
         res = self.default.copy()
         if len(self.candidates) > 0:
-            res.update(random.sample(self.candidates, 1))
+            res.update(random.sample(self.candidates, 1)[0])
+        log.info('responding: %s', res)
         return res
 
 class JSONDefinedHandler(object):
--- logicoma.py
+++ logicoma.py
@@ -4,32 +4,47 @@ import json
 import configparser
 import sys
 import logging
+import handlers
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 class Logicoma(object):
 
-    def __init__(self, token):
+    def __init__(self, config_path='logicoma.ini'):
         self.app = Flask('Logicoma')
         self.app.route('/', methods=['post'])(self.hook)
         self.app.route('/', methods=['get'])(lambda:'hello world')
-        self.token = token
-        self.listen_thread = None
+        self.load_config(config_path)
+
+    def load_config(self, config_path):
+        conf = configparser.ConfigParser()
+        conf.read(config_path)
+        c = conf['logicoma']
+        self.token = c['token']
+        self.debug = c.getboolean('debug')
+        handler_names = map(lambda s: s.strip(), c['handlers'].split(','))
+        sections = map(lambda name: conf['handler_%s' % name], handler_names)
+        self.handlers = [handlers.load_from_config(s) for s in sections]
 
     def run(self, *args, **kwargs):
         return self.app.run(*args, **kwargs)
 
-    def handle(self, text):
+    def dispatch(self, text):
+        for h in self.handlers:
+            response = h.handle(text)
+            if response is not None:
+                response_json = json.dumps(response)
+                return (response_json, 200, {'Content-Type': 'application/json'})
         return ('', 200, {})
 
     def hook(self):
         try:
             payload = request.form
             logger.debug("payload: %s", payload)
-            if payload and payload.get('token') == self.token:
+            if self.debug or payload and payload.get('token') == self.token:
                 if 'text' in payload and payload.get('user_id') != 'USLACKBOT':
-                    return make_response(self.handle(payload.get('text')))
+                    return make_response(self.dispatch(payload.get('text')))
                 else:
                     return Response(status=200)
             else:
@@ -43,9 +58,7 @@ def main(args=sys.argv[1:]):
     config.read('flask.ini')
     server_conf = dict(config['flask'])
     server_conf['port'] = int(server_conf.get('port', '7290'))
-    config = configparser.ConfigParser()
-    conf = dict(config['logicoma'])
-    bot = Logicoma(**conf)
+    bot = Logicoma()
     bot.run(**server_conf)
 
 if __name__ == '__main__':
